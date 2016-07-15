59819ee72fc77e9b3d26c854a0f267b9be24bb71
pballester <pballester@avantio.com>
2016-07-15 11:33:03 +0200

    remove script tags

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.whitelist());
 // server.use(prerender.blacklist());
 server.use(prerender.logger());
-// server.use(prerender.removeScriptTags());
+server.use(prerender.removeScriptTags());
 // server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
