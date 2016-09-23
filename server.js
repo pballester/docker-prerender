aa31e78fd27695f94ffb92573af6cefc12684267
pballester <pballester@avantio.com>
2016-09-23 10:57:11 +0200

    Added some timeouts

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
+'use strict';
+
 var prerender = require('prerender');
 var redisCache = require('./lib/redisCache');
 
 var server = prerender({
     workers: process.env.PRERENDER_NUM_WORKERS,
-    iterations: process.env.PRERENDER_NUM_ITERATIONS
+    iterations: process.env.PRERENDER_NUM_ITERATIONS,
+    resourceDownloadTimeout: process.env.RESOURCE_DOWNLOAD_TIMEOUT || 40000,
+    jsTimeout: process.env.JS_TIMEOUT || 40000
 });
 
 server.use(prerender.sendPrerenderHeader());
