d4d7e8722edb5c9d31191b485cad225e95e2d99e
Pablo Ballester <pabbalca@gmail.com>
2017-12-07 17:38:45 +0100

    pageLoadTimeout in env

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var redisCache = require('./lib/redisCache');
 var viewPort = require('./lib/viewPort');
 
 var server = prerender({
-  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
+  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
+  pageLoadTimeout: process.env.PAGE_LOAD_TIMEOUT || 20 * 1000
 });
 
 server.use(prerender.sendPrerenderHeader());
